81d070bf5846b7403444619463e13dc11335bce2
Ahmad Yar <120385857+ahamdjin@users.noreply.github.com>
2026-09-03 23:15:20 +0500

    Route home journal and contact pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
+import BlogPage from './BlogPage'
+import ContactPage from './ContactPage'
 import './styles.css'
+import './pages.css'
+
+function Router() {
+  const path = window.location.pathname.replace(/\/+$/, '') || '/'
+
+  if (path === '/blog') return <BlogPage />
+  if (path === '/contact') return <ContactPage />
+  return <App />
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Router />
   </StrictMode>,
 )
